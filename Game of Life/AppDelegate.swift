9196aabc8169034b87b95eb0e4467ff1dbda7aed
David Nadoba <dnadoba@gmail.com>
2018-11-11 11:49:03 +0100

    Initial Commit

---
+++ Game of Life/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Game of Life
+//
+//  Created by David Nadoba on 11.11.18.
+//  Copyright © 2018 David Nadoba. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
